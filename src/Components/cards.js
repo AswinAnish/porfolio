29b6a982e59c7792cf497569a6a6c28f78a0f8cf
AswinAnish <91623@Lynx>
2024-09-14 19:35:54 +0530

    EY updated

--- src/Components/cards.js
+++ src/Components/cards.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Cards.css';
 import logo from './f13.png'
+import logo2 from './eylogo.png'
 
 const Cards = () => {
   return (
@@ -9,6 +10,23 @@ const Cards = () => {
       <hr/>
       <div className="card-container">
         {/* Card 1 */}
+        <div className="card custom-card">
+          <img
+            src={logo2} alt={"logo"}
+            className="card-img-top"
+            style={{ height: '200px', objectFit: 'cover' }} />
+          <div className="card-body">
+            <h5 className="card-title">EY GDS</h5>
+            <p className="card-text">Associate Software Engineer</p>
+            {/*<a href="/" className="btn btn-primary">
+      Go somewhere
+    </a>*/}
+          </div>
+        </div>
+      
+
+      
+        {/* Card 2 */}
         <div className="card custom-card">
           <img
             src={logo} alt={"logo"}
@@ -24,6 +42,8 @@ const Cards = () => {
         </div>
       </div>
     </section>
+
+    
     </>
   );
 };
