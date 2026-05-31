481a47e874752e8ebaa027b2196f85ad2ce0c279
Aswin Anish <aswinanish008@gmail.com>
2026-05-31 12:33:35 +0530

    intermediate

--- src/Components/cards.js
+++ src/Components/cards.js
@@ -9,7 +9,7 @@ const Cards = () => {
       <h1>Experience</h1>
       <hr/>
       <div className="card-container">
-        {/* Card 1 */}
+        {/* Card 1 - EY GDS with progression */}
         <div className="card custom-card">
           <img
             src={logo2} alt={"logo"}
@@ -17,10 +17,16 @@ const Cards = () => {
             style={{ height: '200px', objectFit: 'cover' }} />
           <div className="card-body">
             <h5 className="card-title">EY GDS</h5>
-            <p className="card-text">Associate Software Engineer</p>
-            {/*<a href="/" className="btn btn-primary">
-      Go somewhere
-    </a>*/}
+            <div className="experience-stack">
+              <div className="stack-item current">
+                <span className="role-title">Software Engineer</span>
+                <span className="role-duration">Current</span>
+              </div>
+              <div className="stack-item">
+                <span className="role-title">Associate Software Engineer</span>
+                <span className="role-duration">1 Year 5 Months</span>
+              </div>
+            </div>
           </div>
         </div>
       
@@ -35,9 +41,6 @@ const Cards = () => {
           <div className="card-body">
             <h5 className="card-title">F13 Technologies</h5>
             <p className="card-text">AWS Intern for 5 months.</p>
-            {/*<a href="/" className="btn btn-primary">
-      Go somewhere
-    </a>*/}
           </div>
         </div>
       </div>
